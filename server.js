3a0452ebf19c047b0d64d774a451ffa898b863d9
Diego Lopes <diego-chez@hotmail.com>
2021-03-13 16:01:08 -0300

    handle upload with multer

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express")
+const multer = require("multer")
 
 const app = express()
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "uploads/")
+    },
+    filename: function(req, file, cb){
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+const upload = multer({ storage })
 
 app.use(express.static("public"))
+app.post("/file/upload", upload.single("file"), (req, res) => {
+    res.send("<h2>Upload done successfully!<h2>")
+})
 
 app.listen(3000, () => console.log("App running"))
